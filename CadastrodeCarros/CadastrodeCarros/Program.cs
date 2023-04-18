c9a8325c82e107a0e4116f88b3f797047ea46865
marcelo <marcelegald2@outlook.com>
2023-04-18 16:34:08 -0300

    [M2S03] Ex 4 - Crie Instâncias para a classe Proprietario

---
+++ CadastrodeCarros/CadastrodeCarros/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CadastrodeCarros;
+
+namespace CadastrodeCarros
+{
+    internal class Program
+    {
+        // [M2S03] Ex 4 - Crie Instâncias para a classe Proprietario
+        public static void Main(string[] args)
+        {
+            var firstOwner = new Owner("Marcelo", "84578942536", "67984447856");
+            var secondOwner = new Owner("Andre", "45678997745", "488782245889");
+            var thirdOwner = new Owner("Silva", "455477811788", "445858844565");
+        }
+    }   
+}
