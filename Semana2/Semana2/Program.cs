c38499dcaadb22f6e5bb71b3dce8745c296e99d1
marcelo <marcelegald2@outlook.com>
2023-04-18 16:07:27 -0300

    [M2S03] Ex 1 - Criar Método Soma de números

---
+++ Semana2/Semana2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Semana2
+{
+    internal class Semana2
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        // [M2S03] Ex 1 - Criar Método Soma de números
+        public static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
