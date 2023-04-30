c1562b0bce29717527492f35b72cde2f200af255
marcelo <marcelegald2@outlook.com>
2023-04-30 15:50:02 -0300

    [M2S04] Ex 9 - Calculadora: classe estática

---
+++ semana3/semana3/ex-nine/Calculator.cs
@@ -0,0 +1,26 @@
+namespace semana3;
+
+public static class Calculator
+{
+    public const double Pi = 3.14159265359;
+
+    public static double Sum(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtraction(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Product(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Division(double a, double b)
+    {
+        return a / b;
+    }
+}
